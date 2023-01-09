39f9a321179ecbe266a0ce917bcd9f8b3e20a886
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-01-08 19:28:42 -0600

    fixing counter errors

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
 // import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import Counter from '../components/Counter';
+import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  const counters = [{ title: 'Counter 1' }, { title: 'Counter 2' }, { title: 'Counter 3' }, { title: 'Counter 4' }, { title: 'Counter 5' }];
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
+    <>
+      <div>
+        <h1>Hello {user.displayName}! </h1>
+        {counters.map((counter) => (
+          <Counter key={counter.title} title={counter.title} />
+        ))}
+      </div>
+    </>
   );
 }
 
